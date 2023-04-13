d27728d0ed6cf1a6ae8af887bcd57c3963f0e016
Jonas Frost <105543545+analyticsociologyrox@users.noreply.github.com>
2023-04-13 17:52:03 +0200

    rechtschreibung

--- pingpong_1.R
+++ pingpong_1.R
@@ -3,7 +3,7 @@ library(tidyverse)
 ## Hallo Peter! Lass uns die Hausarbeit schreiben!
 
 1 + 1
-## Hallo Jonas! Voll coll! Lass uns direkt anfangen.
+## Hallo Jonas! Voll cool! Lass uns direkt anfangen.
 
 for (i in 1:10) {
   print("hello")
